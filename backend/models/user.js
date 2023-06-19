c097f9b96e8f3825505b503b396513c59ecf0dd3
Elif <2003013054@student.beykent.edu.tr>
2023-06-20 00:10:34 +0300

    backend login-register

---
+++ backend/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+          _id : String,
+          name : {
+                    type : String,
+                    required : true,
+          },
+          email : {
+                    type : String,
+                    required : true,
+                    unique : true,
+          },
+          password : {
+                    type : String,
+                    required : true,
+          },
+          isAdmin : Boolean,
+          createdDate : Date
+});
+
+const user = mongoose.model('user', userSchema);
+
+module.exports = user;
